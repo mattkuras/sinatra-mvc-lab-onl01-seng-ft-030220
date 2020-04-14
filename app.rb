eb50f6aaf0b173577844a037eeb3e9726c771f70
matt_kuras <mattkuras@gmail.com>
2020-04-14 18:08:28 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
+require_relative 'piglatinizer.rb'
 
 class App < Sinatra::Base
+    get '/' do
+        erb :user_input 
+    end
+
+    post '/pig' do 
+        erb :display_pig_latin
+    end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,12 @@
+class PigLatinizer 
+    attr_reader :text
+
+    def initialize(text)
+      @text = text.downcase
+    end
+
+    def pig_latin
+        @text. 
+    end
+
+end
